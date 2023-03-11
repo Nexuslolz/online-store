972103d86d5f1ee835ca0d97d94f2d59fcb6bd23
Nexuslolz <a.klyosov@inbox.ru>
2023-03-11 07:56:12 +0500

    feat: add search func

--- src/components/Search/SearchMain.tsx
+++ src/components/Search/SearchMain.tsx
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
+
+import { useDispatch } from 'react-redux';
 
 import styles from './SearchMain.module.scss';
 
+import { useLazyFetchDifiniteProductQuery } from '../../store/services/productService.api';
+import { searchSlice } from '../../store/slices/searchSlice';
 import SearchInput from '../SearchInput/SearchInput';
 
 const SearchMain = () => {
   const [searchValue, setSearchValue] = useState<string>('');
   const [active, setActive] = useState<boolean>(false);
-  // const setSearchValue = () => {};
 
-  const resetSearchValue = () => {
+  const [getProduct, { error }] = useLazyFetchDifiniteProductQuery();
+  const dispatch = useDispatch();
+
+  const resetSearchValue = useCallback(() => {
     setSearchValue('');
-  };
+    dispatch(searchSlice.actions.resetSearchValue());
+  }, [dispatch]);
 
-  const submitHandler = (event: React.FormEvent) => {
+  const submitHandler = async (event: React.FormEvent) => {
     event.preventDefault();
-    console.log('send search');
+    setSearchValue(searchValue);
+
+    const data = await getProduct(searchValue);
+    if (!data.data) return;
+    dispatch(searchSlice.actions.setSearchValue(data.data.products));
+    if (error) {
+      dispatch(searchSlice.actions.setSearchError(error));
+    }
   };
 
   return (
