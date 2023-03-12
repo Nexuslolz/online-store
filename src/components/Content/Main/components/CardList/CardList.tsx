a53a9fcb7cfb43ac8facf50eacbc8ff9b2a1d0d8
Nexuslolz <a.klyosov@inbox.ru>
2023-03-12 18:25:21 +0500

    feat: update cardlist

--- src/components/Content/Main/components/CardList/CardList.tsx
+++ src/components/Content/Main/components/CardList/CardList.tsx
@@ -4,7 +4,9 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import styles from './CardList.module.scss';
 
+import { IProduct } from '../../../../../models/models';
 import { getIsList } from '../../../../../store/selectors/listSelector';
+import { getSearchParams } from '../../../../../store/selectors/searchSelector';
 import { useFetchProductsQuery } from '../../../../../store/services/productService.api';
 
 import { totalSlice } from '../../../../../store/slices/totalSlice';
@@ -12,15 +14,19 @@ import Card from '../../../../Card/Card';
 import ListCard from '../../../../Card/ListCard/ListCard';
 import Loader from '../../../../Loader/MainLoader/MainLoader';
 
-const CardList = () => {
+const CardList: React.FC = () => {
   const { data, isLoading, error } = useFetchProductsQuery('100');
-  const isList = useSelector(getIsList);
+  const isList: boolean = useSelector(getIsList);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(totalSlice.actions.setAmount(String(data?.products.length)));
   }, [data, dispatch]);
+
+  const [searchData, searchLoading, searchError] = useSelector(getSearchParams);
+
+  const productData: IProduct[] | undefined = searchData ?? data?.products;
 
   return (
     <div
@@ -34,16 +40,18 @@ const CardList = () => {
           : styles.mainContainer__cardList
       }
     >
-      {/* {error && (
-        <h1 className={styles.mainContainer__error}>
-          Error has occured, {error.error}, {error.status}
-        </h1>
-      )} */}
-      {error && <Loader />}
-      {isLoading ? (
+      {isLoading || searchLoading ? (
         <Loader />
+      ) : error ? (
+        <h1 className={styles.mainContainer__error}>
+          Error has occured. {'status' in error ? error.status : error.message}. Try again later.
+        </h1>
+      ) : searchError ? (
+        <h1 className={styles.mainContainer__error}>Error has occured, {searchError}</h1>
+      ) : productData?.length === 0 ? (
+        <h1 className={styles.mainContainer__noFound}>There is no products found &#128549;</h1>
       ) : (
-        data?.products.map((product) =>
+        productData?.map((product) =>
           isList ? (
             <ListCard
               description={product.description}
