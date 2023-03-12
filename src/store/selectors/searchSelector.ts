bbd7b93d9ba880a43799f3ba43b8aec5cfa9487c
Nexuslolz <a.klyosov@inbox.ru>
2023-03-12 18:26:16 +0500

    feat: create search selector to getting data

--- src/store/selectors/searchSelector.ts
+++ src/store/selectors/searchSelector.ts
@@ -1,5 +1,10 @@
+import { IProduct } from '../../models/models';
 import { RootState } from '../store';
 
-export const getSearchValue = (state: RootState) => {
-  return state.search.searchValue;
+export const getSearchParams = (state: RootState): [IProduct[] | null, boolean, string] => {
+  const searchData: IProduct[] | null = state.search.searchValue;
+  const searchLoading: boolean | undefined = state.search.loading;
+  const searchError = state.search.error;
+
+  return [searchData, searchLoading, searchError];
 };
