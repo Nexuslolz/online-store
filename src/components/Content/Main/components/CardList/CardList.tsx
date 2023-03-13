4c16fbb8e46a71cb2228c5cbe9817329f9c191a3
Nexuslolz <a.klyosov@inbox.ru>
2023-03-13 16:03:36 +0500

    feat: add sorting

--- src/components/Content/Main/components/CardList/CardList.tsx
+++ src/components/Content/Main/components/CardList/CardList.tsx
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
 import styles from './CardList.module.scss';
 
 import { IProduct } from '../../../../../models/models';
+import { getData } from '../../../../../store/selectors/dataSelector';
 import { getIsList } from '../../../../../store/selectors/listSelector';
 import { getSearchParams } from '../../../../../store/selectors/searchSelector';
 import { useFetchProductsQuery } from '../../../../../store/services/productService.api';
 
+import { dataSlice } from '../../../../../store/slices/dataSlice';
 import { totalSlice } from '../../../../../store/slices/totalSlice';
 import Card from '../../../../Card/Card';
 import ListCard from '../../../../Card/ListCard/ListCard';
@@ -27,6 +29,13 @@ const CardList: React.FC = () => {
   const [searchData, searchLoading, searchError] = useSelector(getSearchParams);
 
   const productData: IProduct[] | undefined = searchData ?? data?.products;
+
+  // const [products, setProducts] = useState<IProduct[] | undefined>(productData!);
+  // dispatch(dataSlice.actions.setData(productData));
+
+  // setProducts(useSelector(getData));
+  // console.log(products);
+  // setProducts(dataRes);
 
   return (
     <div
