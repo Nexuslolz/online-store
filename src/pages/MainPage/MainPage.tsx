b7fb1e2b4ae8e15cad9a05b6c01d69bd0aa89384
Nexuslolz <a.klyosov@inbox.ru>
2023-03-21 01:13:21 +0500

    feat: add opening filter panel from aside, add adaptive styles

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import styles from './MainPage.module.scss';
 
 import MainContent from '../../components/Content/Main/MainContent';
 import FilterPanel from '../../components/Filter/FilterPanel';
+import { getIsOpen } from '../../store/selectors/menuSelector';
+import { menuSlice } from '../../store/slices/menuSlice';
 
 const MainPage: React.FC = () => {
+  const dispatch = useDispatch();
+  const isOpen = useSelector(getIsOpen);
+
+  const openMenu = () => {
+    if (!isOpen) {
+      dispatch(menuSlice.actions.setMenu(true));
+    }
+  };
+
   return (
     <main className={styles.main}>
+      <div onClick={openMenu} className={styles.filterBtn__wrapper}>
+        <button className={styles.filter__btn}>{'>'}</button>
+      </div>
       <FilterPanel />
       <MainContent />
     </main>
